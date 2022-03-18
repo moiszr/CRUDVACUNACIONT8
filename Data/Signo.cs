477b43a3aea3dc10e4176def197beb51580ad8ee
Danielpba02 <pereyradaniel2020@gmail.com>
2022-03-18 19:24:04 -0400

    Agregar lista de signo

---
+++ Data/Signo.cs
@@ -0,0 +1,17 @@
+﻿namespace CRUDVACUNACIONT8.Data
+{
+    public class Signo
+    {
+        public int Id { get; set; }
+        public string? Cedula { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Telefono { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int IdProvincia { get; set; }
+        public Provincias? Provincias { get; set; }
+        public int IdVacuna { get; set; }
+        public VacunasRecibidas? VacunasRecibidas { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
