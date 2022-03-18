cc7c65b77eeec9d452a460a10f2b73f9004372b0
moiszr <moisesnzr.rd@gmail.com>
2022-03-18 18:51:08 -0400

    Organizacion de configuracion y creacion de Acercade.razor

--- Data/CrudDbContext.cs
+++ Data/CrudDbContext.cs
@@ -4,9 +4,9 @@ namespace CRUDVACUNACIONT8.Data
 {
     public class CrudDbContext : DbContext
     {
-        public DbSet<Provincias>? Provincias { get; set; }
-        public DbSet<VacunasRecibidas>? Vacunas { get; set; }
-        public DbSet<Personas>? Personas { get; set; }
+        public DbSet<Provincias> Provincias { get; set; } = default!;
+        public DbSet<VacunasRecibidas> Vacunas { get; set; } = default!;
+        public DbSet<Personas> Personas { get; set; } = default!;
         public CrudDbContext(DbContextOptions<CrudDbContext> options) : base(options) { }
     }
 }
